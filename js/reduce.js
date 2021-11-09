90d634a751a92a7d7fce9a310af005f1f40905e4
Marco Messina <voldrak94@gmail.com>
2021-11-09 17:24:07 +0100

    completato

--- js/reduce.js
+++ js/reduce.js
@@ -3,8 +3,11 @@ function scalculator(numbers) {
     const sub = () => numbers.reduce ((acc, num) => acc - num);
     const mult = () => numbers.reduce ((acc, num) => acc * num);
     const div = () => numbers.reduce ((acc, num) => acc / num);
-    const pow = () => numbers.map (num =>Math.pow(num,2));
-    const sqrt = () => numbers.map (num => Math.sqrt (num));
+    const pow = () => numbers.reduce ((acc, num) => (acc[num] = Math.pow(num, 2), acc), {});
+    // const pow = () => numbers.map (num =>Math.pow(num,2));
+
+    // const sqrt = () => numbers.map (num => Math.sqrt (num));
+    const sqrt = () => numbers.reduce ((acc, num) => (acc[num] = Math.sqrt(num, 2), acc), {});
 
     return {
         sum: sum(),
